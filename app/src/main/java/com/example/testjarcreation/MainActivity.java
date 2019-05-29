a0f42648fd6ad6b1800cd6f0c09d3279493b7190
saifreza <reza@live-mediabd.net>
2019-05-29 18:04:08 +0600

    first commit

---
+++ app/src/main/java/com/example/testjarcreation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.testjarcreation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.testlibrary.TestActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // startActivity(new Intent(this, TestActivity.class));
+    }
+}
