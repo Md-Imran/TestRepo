ea644b9cc99af298ef87787d7cec0f595b292841
saifreza <reza@live-mediabd.net>
2019-05-29 21:46:29 +0600

    add toast message

--- testlibrary/src/main/java/com/example/testlibrary/TestActivity.java
+++ testlibrary/src/main/java/com/example/testlibrary/TestActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -20,8 +21,10 @@ public class TestActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Button Click", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                Toast.makeText(TestActivity.this,"Button Click",Toast.LENGTH_LONG).show();
             }
         });
     }
